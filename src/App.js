15dd6c6edb73dca7c9328c94dc4208cbcff282ac
Yuri Alves <yurialves2@gmail.com>
2019-06-02 23:47:10 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from './components/Header'
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-    <h1>Olá, Mundo!</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Header/>
+  </div>
+);
+
 
 export default App;
